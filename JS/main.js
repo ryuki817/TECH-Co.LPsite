543447e6f044a767ceea0f71a82bdbbfbec1b2da
Ryuki <o.ryuki0817@i.softbank.jp>
2025-06-05 21:59:59 +0900

    0605 update+1

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,34 @@
 "use strict";
+
+/*---------- GSAP 設定 ----------*/
+
+//存在しない要素を取得しようとするときに出るエラーを非表示にする
+gsap.config({
+    nullTargetWarn: false,
+  });
+
+
+
+// スライドイン  
+gsap.fromTo(
+    ".js_service",
+    {
+      y: 200,   
+      autoAlpha: 0,
+    },
+    {
+      y: 0,
+      autoAlpha: 1,
+      duration: 0.8,
+      ease: "Power4.inOut",
+      scrollTrigger: {
+        trigger: "#course",
+        start: "top top",
+        // markers:true,
+      },
+      stagger: {
+        each: 0.2,
+        from: "start",
+      },
+    }
+  );
